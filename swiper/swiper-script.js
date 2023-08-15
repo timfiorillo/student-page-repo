0cb54cbe7e8daa13ff1c70e5d544d4290d74e20c
Tim Fiorillo <30870048+timfiorillo@users.noreply.github.com>
2023-08-15 09:44:11 -0400

    Add files to repo

---
+++ swiper/swiper-script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+  grabCursor: true,
+  slidesPerView: 'auto',
+  spaceBetween: 25,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    dragSize: 'auto',
+  },
+
+
+
+});
