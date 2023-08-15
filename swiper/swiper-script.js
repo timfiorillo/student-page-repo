8b44013895ce69bc9b092eb521c45ed59d18ee82
Tim Fiorillo <30870048+timfiorillo@users.noreply.github.com>
2023-08-15 17:02:36 -0400

    styling + responsiveness
    Update styling and responsiveness

--- swiper/swiper-script.js
+++ swiper/swiper-script.js
@@ -9,6 +9,32 @@ var swiper = new Swiper('.swiper-container', {
     dragSize: 'auto',
   },
 
+  breakpoints: {
+    // when window width is >= 650px
+    550: {
+      slidesPerView: 2.2,
+      spaceBetween: 5
+    },
+    // when window width is >= 650px
+    650: {
+      slidesPerView: 2.5,
+      spaceBetween: 5,
+      scrollbar: {
+        draggable: true
+      },
+    },
+    // when window width is >= 730px
+    730: {
+      slidesPerView: 3,
+      spaceBetween: 5
+    },
+    // when window width is >= 849px
+    849: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    }
+  }
+
 
 
 });
